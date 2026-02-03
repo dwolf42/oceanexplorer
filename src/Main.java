714b95db912de92eabf323c8ccdc557a0d771838
dwolf <46925680+dwolf42@users.noreply.github.com>
2026-02-03 11:12:43 +0100

    feat: add new Ship object

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import explorer.ShipApp;
+import ocean.Vec2D;
 
 public class Main {
 	public static void main(String[] args) {
-		ShipApp shipApp = new ShipApp();
+		ShipApp shipApp = new ShipApp("127.0.0.1",
+			8150,
+			"The Ship",
+			"ship",
+			new Vec2D(0, 0),
+			new Vec2D(1, 1));
 	}
 }
