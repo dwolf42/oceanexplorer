38f677adb21cafee39fb4d908eec74643b599ac0
dwolf <46925680+dwolf42@users.noreply.github.com>
2026-02-10 16:48:27 +0100

    wip refactor: move reader writer to oceanlistener

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import explorer.ShipApp;
 import ocean.Vec2D;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		ShipApp shipApp = new ShipApp("127.0.0.1",
-			8150,
-			"The Ship",
-			"ship",
-			new Vec2D(0, 0),
-			new Vec2D(1, 1));
+		ShipApp shipApp = new ShipApp("The Ship", "ship");
+
+		// establish connection to OceanServer
+		if (shipApp.connectOS("127.0.0.1", 8150)) {
+			System.out.println("Connected to OceanServer");
+			shipApp.launch(new Vec2D(0,0), new Vec2D(1, 1));
+
+		} else {
+			System.out.println("Failed to connect to OceanServer");
+		}
+
+/*
+shipApp.launch()
+launch fragt mich dann, welche Koordinaten und welche Ausrichtung
+
+
+ */
 	}
 }
