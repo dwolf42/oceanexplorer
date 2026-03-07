86bab3f37fa8d4bac8505b4feddc506e7c09e25e
Whaaat761 <katarina.tonkovic555@gmail.com>
2026-03-07 21:01:36 +0100

    feat: Add database and web application functionalities
    !! Important: Before testing, the database credentials must be entered in the Database.java file

---
+++ src/explorer/Main.java
@@ -0,0 +1,8 @@
+package explorer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        WebApp app = new WebApp();
+        app.startWebApplication();
+    }
+}
