0a3c054035fefdd7debf0d98f719ec671fe0aa23
David Wolf <46925680+dwolf42@users.noreply.github.com>
2026-03-08 21:16:51 +0100

    wip feat: add skeleton for submarines

---
+++ resources/ocean_explorer_v1.4/ocean/Course.java
@@ -0,0 +1,14 @@
+package ocean;
+
+// Fahrtrichtung von Schiffen  
+
+//Alle enums bieten automatisch folgenden Methoden:
+//String name() - Liefert der Enum-Wert als String
+//int ordinal() - Liefert den Enum-Wert als Zahl
+//EnumObj EnumName.valueOf(String name) - wandelt einen String in Enum-Wert um
+
+public enum Course {
+	Forward,	// vorwärts fahren 
+	Backward 	// rückwärts fahren 
+}
+
