de92df89d497b590daac151c208e152de8b63d45
dwolf <46925680+dwolf42@users.noreply.github.com>
2026-03-09 18:15:23 +0100

    wip feat: implement submarine and submarine server

--- src/explorer/Explorer.java
+++ src/explorer/Explorer.java
@@ -1,49 +1,67 @@
 package explorer;
 
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Explorer {
 	private boolean isExploring = true;
+
 	public static void main(String[] args) throws InterruptedException {
 		Explorer exp = new Explorer();
-		exp.explore();
+		try {
+			exp.explore();
+		} catch (InterruptedException e) {
+			// -
+		}
 	}
 
 	public void explore() throws InterruptedException {
 		ShipApp shipApp = new ShipApp("The Ship", "ship");
 
-		// establish connection to OceanServer
-		if (shipApp.connectOS("127.0.0.1", 8150)) {
-			System.out.println("Connected to OceanServer");
-			shipApp.launch();
-		} else {
-			System.out.println("Failed to connect to OceanServer");
+		synchronized (shipApp) {
+			// establish connection to OceanServer
+			if (shipApp.connectOS("127.0.0.1", 8150)) {
+				System.out.println("Connected to OceanServer");
+				shipApp.launch();
+				shipApp.wait();
+			} else {
+				System.out.println("Failed to connect to OceanServer");
+			}
 		}
 
 		Scanner scanner = new Scanner(System.in);
 
 		String input;
-		while (isExploring) {
-			System.out.println("System waiting for input:");
-			input = scanner.nextLine().toLowerCase();
+		while (isExploring && !Thread.currentThread().isInterrupted()) {
+			synchronized (shipApp) {
 
-			switch (input) {
-				case "scan":
-					shipApp.scan();
-					break;
-				case "navigate":
-					shipApp.navigate();
-					break;
-				case "radar":
-					shipApp.radar();
-					break;
-				case "exit":
-					isExploring = false;
-					shipApp.exit();
-					break;
-				default:
-					System.out.println("Invalid input");
-					break;
+				System.out.println("System waiting for input:");
+				input = scanner.nextLine().toLowerCase();
+
+				switch (input) {
+					case "scan":
+						shipApp.scan();
+						shipApp.wait();
+						break;
+					case "navigate":
+						shipApp.navigate();
+						shipApp.wait();
+						break;
+					case "radar":
+						shipApp.radar();
+						shipApp.wait();
+						break;
+					case "sub":
+						shipApp.sub();
+						break;
+					case "exit":
+						isExploring = false;
+						shipApp.exit();
+						break;
+					default:
+						System.out.println("Invalid input");
+						break;
+				}
 			}
 		}
 	}
