a3bc6c0b1f70076a37da68a4edd128e11f112dca
Whaaat761 <katarina.tonkovic555@gmail.com>
2026-03-11 22:32:17 +0100

    feat: Add database and web application functionalities

---
+++ src/explorer/Database.java
@@ -0,0 +1,342 @@
+package explorer;
+
+import ocean.Vec2D;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database {
+    private Connection conn;
+    private Statement statement;
+    private PreparedStatement stmt;
+
+    // Database connection parameters
+    // !!!
+    // TODO: DB_URL and PASS should be adjusted depending on the real credentials for the database
+    // !!!
+    static final String DB_URL = "jdbc:mariadb://localhost:3307/ocean_explorer";
+    static final String USER = "root";
+    static final String PASS = "OvoJeSamoPswZaMariaDB5.(0)!";
+
+    public Database() throws SQLException {
+        this.conn = this.getConnection();
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
+    public synchronized int insertShipData(String shipIdentifierFromServer, String shipName) throws SQLException {
+        String sql = "INSERT INTO Ship (name, active, server_ship_id) " +
+                "VALUES ('" + shipName + "', 'Yes', '" + shipIdentifierFromServer + "')";
+
+        statement = conn.createStatement();
+        int shipDatabaseIdentifier = statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+
+        statement.close();
+        return shipDatabaseIdentifier;
+    }
+
+    public synchronized void insertShipScanData(int totalDepthAverage,
+                                                float standardDeviation,
+                                                Vec2D sectorCoordinates,
+                                                int shipDatabaseIdentifier) throws SQLException {
+
+        String sql = "INSERT INTO scan_results (total_depth_average, standard_deviation) " +
+                "VALUES (?, ?)";
+
+        stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        stmt.setInt(1, totalDepthAverage);
+        stmt.setFloat(2, standardDeviation);
+        stmt.executeUpdate();
+
+        int scanResultID = getScanResultID(stmt.getGeneratedKeys());
+        int sectorID = getSectorID(sectorCoordinates);
+
+        String sql2 = "INSERT INTO ship_scan_results (scan_resultID, sectorID, shipID) " +
+                "VALUES (?, ?, ?)";
+
+        stmt = conn.prepareStatement(sql2);
+
+        stmt.setInt(1, scanResultID);
+        stmt.setInt(2, sectorID);
+        stmt.setInt(3, shipDatabaseIdentifier);
+        int i = stmt.executeUpdate();
+
+        stmt.close();
+    }
+
+    private int getSectorID(Vec2D sector) throws SQLException {
+        int positionX = sector.getX();
+        int positionY = sector.getY();
+
+        String sql = "SELECT sectorID FROM sector WHERE position_x = ? AND position_y = ?";
+
+        stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, positionX);
+        stmt.setInt(2, positionY);
+        ResultSet rs = stmt.executeQuery();
+
+        int sectorID = 0;
+
+        while (rs.next()) {
+            sectorID = rs.getInt("sectorID");
+        }
+
+        statement.close();
+        return sectorID;
+    }
+
+    private synchronized int getScanResultID(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            int id = rs.getInt(1);
+            return id;
+        }
+        return 0;
+    }
+
+
+    public synchronized List<Map<String, Object>> getAllShips() throws SQLException {
+        List<Map<String, Object>> ships = new ArrayList<>();
+        String sql = "SELECT * FROM ship";
+
+        conn = getConnection();
+        statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+
+        while (rs.next()) {
+            Map<String, Object> ship = new HashMap<>();
+            ship.put("shipID", rs.getInt("shipID"));
+            ship.put("name", rs.getString("name"));
+            ship.put("typ", rs.getString("typ"));
+            ship.put("active", rs.getString("active"));
+            ships.add(ship);
+        }
+
+        conn.close();
+        statement.close();
+        return ships;
+    }
+
+    public synchronized List<Map<String, Object>> getAllSectors() throws SQLException {
+        List<Map<String, Object>> sectors = new ArrayList<>();
+        String sql = "SELECT * FROM sector";
+
+        conn = getConnection();
+        statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+
+        while (rs.next()) {
+            Map<String, Object> sector = new HashMap<>();
+            sector.put("sectorID", rs.getInt("sectorID"));
+            sector.put("position_x", rs.getInt("position_x"));
+            sector.put("position_y", rs.getInt("position_y"));
+            sectors.add(sector);
+        }
+
+        conn.close();
+        statement.close();
+        return sectors;
+    }
+
+    public synchronized List<Map<String, Object>> getShipScanData(int identifier) throws SQLException {
+        List<Map<String, Object>> shipScanData = new ArrayList<>();
+        String sql = "SELECT * FROM ship_scan_results WHERE shipID = ? ";
+
+        conn = getConnection();
+        stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, identifier);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Map<String, Object> shipData = new HashMap<>();
+            int scan_resultID = rs.getInt("scan_resultID");
+            int sectorID = rs.getInt("sectorID");
+            shipData.put("sectorID", sectorID);
+
+            String sql2 = "SELECT total_depth_average, standard_deviation FROM scan_results WHERE scan_resultID = ?";
+            stmt = conn.prepareStatement(sql2);
+            stmt.setInt(1, scan_resultID);
+            ResultSet rs2 = stmt.executeQuery();
+
+            while (rs2.next()) {
+                shipData.put("total_depth_average", rs2.getInt("total_depth_average"));
+                shipData.put("standard_deviation", rs2.getDouble("standard_deviation"));
+            }
+
+            shipScanData.add(shipData);
+        }
+
+        conn.close();
+        stmt.close();
+        return shipScanData;
+    }
+
+    public synchronized List<Map<String, Object>> getAllSectorData() throws SQLException {
+        List<Map<String, Object>> sectors = getAllSectors();
+        List<Map<String, Object>> allSectorData = new ArrayList<>();
+
+        for (Map<String, Object> sector : sectors) {
+            int sectorID = (Integer) sector.get("sectorID");
+
+            Map<String, Object> scanData = getScanResultsForSector(sectorID);
+            sector.putAll(scanData);
+
+            Map<String, Object> radarData = getRadarResultsForSector(sectorID);
+            sector.putAll(radarData);
+
+            allSectorData.add(sector);
+        }
+        return allSectorData;
+    }
+
+    private synchronized Map<String, Object> getScanResultsForSector(int sectorID) throws SQLException {
+        Map<String, Object> result = new HashMap<>();
+        String sql = """
+        SELECT sr.total_depth_average, sr.standard_deviation
+        FROM ship_scan_results ssr
+        JOIN scan_results sr ON ssr.scan_resultID = sr.scan_resultID
+        WHERE ssr.sectorID = ?
+        LIMIT 1
+    """;
+
+        Connection conn = getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, sectorID);
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            result.put("total_depth_average", rs.getInt("total_depth_average"));
+            result.put("standard_deviation", rs.getDouble("standard_deviation"));
+        }
+
+        rs.close();
+        stmt.close();
+        conn.close();
+
+        return result;
+    }
+
+    private synchronized Map<String, Object> getRadarResultsForSector(int sectorID) throws SQLException {
+        Map<String, Object> result = new HashMap<>();
+        String sql = """
+        SELECT rr.height, rr.ground, rr.navigable
+        FROM radar_results rr
+        JOIN ship_radar_results shipRadarResults ON rr.radar_resultID = shipRadarResults.ship_radar_resultID
+        WHERE shipRadarResults.sectorID = ?
+        LIMIT 1
+    """;
+
+        Connection conn = getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, sectorID);
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            result.put("height", rs.getInt("height"));
+            result.put("ground", rs.getString("ground"));
+            result.put("navigable", rs.getString("navigable"));
+        }
+
+        rs.close();
+        stmt.close();
+        conn.close();
+
+        return result;
+    }
+
+    public synchronized List<Map<String, Object>> getSubmarineMeasurementsForSector(int sectorID) throws SQLException {
+        List<Map<String, Object>> result = new ArrayList<>();
+        String sql = "SELECT submarineID, vec_x, vec_y, vec_z FROM submarine_measurements WHERE sectorID = ?";
+
+        Connection conn = getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, sectorID);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Map<String, Object> row = new HashMap<>();
+            row.put("submarineID", rs.getInt("submarineID"));
+            row.put("vec_x", rs.getInt("vec_x"));
+            row.put("vec_y", rs.getInt("vec_y"));
+            row.put("vec_z", rs.getInt("vec_z"));
+            result.add(row);
+        }
+
+        rs.close();
+        stmt.close();
+        conn.close();
+
+        return result;
+    }
+
+    public synchronized List<Map<String, Object>> getShipRadarData(String shipID) throws SQLException {
+        List<Map<String, Object>> shipRadarData = new ArrayList<>();
+        String sql = "SELECT * FROM ship_radar_results WHERE shipID = ?";
+
+        conn = getConnection();
+        stmt = conn.prepareStatement(sql);
+        stmt.setString(1, shipID);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Map<String, Object> shipData = new HashMap<>();
+            int radar_resultID = rs.getInt("radar_resultID");
+            int sectorID = rs.getInt("sectorID");
+            shipData.put("sectorID", sectorID);
+
+            String sql2 = "SELECT height, ground, navigable FROM radar_results WHERE radar_resultID = ?";
+            stmt = conn.prepareStatement(sql2);
+            stmt.setInt(1, radar_resultID);
+            ResultSet rs2 = stmt.executeQuery();
+
+            while (rs2.next()) {
+                shipData.put("height", rs2.getInt("height"));
+                shipData.put("ground", rs2.getString("ground"));
+                shipData.put("navigable", rs2.getString("navigable"));
+            }
+            shipRadarData.add(shipData);
+        }
+        conn.close();
+        stmt.close();
+        return shipRadarData;
+    }
+
+    public synchronized List<Map<String, Object>> getAllSubmarineData() throws SQLException {
+        List<Map<String, Object>> submarines = new ArrayList<>();
+        String sql = "SELECT s.submarineID, ship.`name` AS ship_name," +
+                "ssp.position_x AS sink_position_x, ssp.position_y AS sink_position_y, ssp.position_z AS sink_position_z," +
+                "sap.position_x AS arise_position_x, sap.position_y AS arise_position_y, sap.position_z AS arise_position_z," +
+                "s.`active`, s.sunk " +
+                "FROM submarine s " +
+                "INNER JOIN `ship` ON s.shipID = ship.shipID " +
+                "INNER JOIN `submarine_sink_position` ssp ON s.sink_positionID = ssp.sink_positionID " +
+                "INNER JOIN `submarine_arise_position` sap ON s.arise_positionID = sap.arise_positionID";
+
+        conn = getConnection();
+        statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+
+        while (rs.next()) {
+            Map<String, Object> submarine = new HashMap<>();
+            submarine.put("submarineID", rs.getInt("submarineID"));
+            submarine.put("shipName", rs.getString("ship_name"));
+            submarine.put("sinkPositionX", rs.getInt("sink_position_x"));
+            submarine.put("sinkPositionY", rs.getInt("sink_position_y"));
+            submarine.put("sinkPositionZ", rs.getInt("sink_position_z"));
+            submarine.put("arisePositionX", rs.getInt("arise_position_x"));
+            submarine.put("arisePositionY", rs.getInt("arise_position_y"));
+            submarine.put("arisePositionZ", rs.getInt("arise_position_z"));
+            submarine.put("active", rs.getString("active"));
+            submarine.put("sunk", rs.getString("sunk"));
+            submarines.add(submarine);
+        }
+
+        conn.close();
+        statement.close();
+        return submarines;
+    }
+}
---
+++ src/explorer/WebApp.java
@@ -0,0 +1,287 @@
+package explorer;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class WebApp {
+    private Database database = new Database();
+    private List<Map<String, Object>> ships;
+    private List<Map<String, Object>> allSectorsData;
+    private List<Map<String, Object>> shipsScanData;
+    private List<Map<String, Object>> shipsRadarData;
+    private List<Map<String, Object>> submarinesData;
+
+    public WebApp() throws SQLException {
+    }
+
+    public void startWebApplication() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", this::handleRequest);
+        server.createContext("/static/", this::handleStaticFiles);
+        server.start();
+        System.out.println("Open http://localhost:8000/");
+    }
+
+    public void handleRequest(HttpExchange exchange){
+        try {
+            String path = exchange.getRequestURI().getPath();
+
+            if (path.equals("/")) {
+                renderHomepage(exchange, "homepage.html");
+            } else if (path.equals("/ships")) {
+                renderShips(exchange);
+            } else if (path.equals("/sectors")) {
+                renderSectorData(exchange);
+            } else if (path.equals("/submarines")) {
+                renderSubmarineData(exchange);
+            }
+            else if (path.contains("/scan-results") && path.contains("/ship")) {
+                String[] parts = path.split("/");
+                int shipID = Integer.parseInt(parts[2]);
+                renderShipScanData(exchange, shipID);
+            }
+            else if (path.contains("/radar-results") && path.contains("/ship")) {
+                String[] parts = path.split("/");
+                String shipID = parts[2];
+                renderShipRadarData(exchange, shipID);
+            }
+            else if (path.contains("/sector") && path.contains("/submarine-measurements") ) {
+                String[] parts = path.split("/");
+                int sectorID = Integer.parseInt(parts[2]);
+                renderSectorSubmarineMeasurements(exchange, sectorID);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleStaticFiles(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        String filePath = "src/explorer/view" + path;
+
+        File file = new File(filePath);
+        if (!file.exists()) {
+            exchange.sendResponseHeaders(404, 0);
+            exchange.getResponseBody().close();
+            return;
+        }
+
+        String contentType = path.endsWith(".css") ? "text/css" : "text/plain";
+        exchange.getResponseHeaders().add("Content-Type", contentType);
+        byte[] bytes = java.nio.file.Files.readAllBytes(file.toPath());
+        exchange.sendResponseHeaders(200, bytes.length);
+        exchange.getResponseBody().write(bytes);
+        exchange.getResponseBody().close();
+    }
+
+    private String getHTMLBase(String htmlTemplateName, StringBuilder rows){
+        StringBuilder data = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader("src/explorer/view/templates/" + htmlTemplateName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return data.toString().formatted(rows);
+    }
+
+    private void renderSectorSubmarineMeasurements(HttpExchange exchange, int sectorID) throws SQLException, IOException {
+        submarinesData = database.getSubmarineMeasurementsForSector(sectorID);
+        StringBuilder rows = new StringBuilder();
+        for (Map<String, Object> submarineData : submarinesData) {
+            rows.append(String.format("""
+            <tr>
+                <td data-label='Submarine ID'>%s</td>
+                <td data-label='Vec X'>%s</td>
+                <td data-label='Vec Y'>%s</td>
+                <td data-label='Vec Z'>%s</td>
+            </tr>
+            """,
+                    submarineData.get("submarineID"),
+                    submarineData.get("vec_x"),
+                    submarineData.get("vec_y"),
+                    submarineData.get("vec_z")
+            ));
+        }
+
+        String html = getHTMLBase("sectorSubmarineData.html", rows);
+        renderHTML(exchange, html);
+    }
+
+    private void renderSectorData(HttpExchange exchange) throws IOException, SQLException {
+        allSectorsData = database.getAllSectorData();
+        StringBuilder rows = new StringBuilder();
+        for (Map<String, Object> sector : allSectorsData) {
+            rows.append(String.format("""
+            <tr>
+                <td data-label='Sector ID'>%s</td>
+                <td data-label='Position X'>%s</td>
+                <td data-label='Position Y'>%s</td>
+                <td data-label='Depth Average'>%s</td>
+                <td data-label='Standard Deviation'>%s</td>
+                <td data-label='Height'>%s</td>
+                <td data-label='Ground'>%s</td>
+                <td data-label='Navigable'>%s</td>
+                <td data-label='Submarine depth measurements'>
+                    <a class="back-button" href="http://localhost:8000/sector/%s/submarine-measurements">Show submarine data</a>
+                </td>
+            </tr>
+            """,
+                    sector.get("sectorID"),
+                    sector.get("position_x"),
+                    sector.get("position_y"),
+                    sector.getOrDefault("total_depth_average", "No Data"),
+                    sector.getOrDefault("standard_deviation", "No Data"),
+                    sector.getOrDefault("height", "No Data"),
+                    sector.getOrDefault("ground", "No Data"),
+                    sector.getOrDefault("navigable", "No Data"),
+                    sector.get("sectorID")
+            ));
+        }
+
+        String html = getHTMLBase("sectorData.html", rows);
+        renderHTML(exchange, html);
+    }
+
+    private void renderHomepage(HttpExchange exchange, String htmlTemplateName) throws SQLException, IOException {
+        StringBuilder data = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/explorer/view/templates/" + htmlTemplateName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String html = data.toString();
+        renderHTML(exchange, html);
+    }
+
+    private void renderHTML(HttpExchange exchange, String html) throws IOException {
+        exchange.getResponseHeaders().add("Content-Type", "text/html; charset=UTF-8");
+        exchange.sendResponseHeaders(200, html.getBytes("UTF-8").length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(html.getBytes("UTF-8"));
+        os.close();
+    }
+
+    private void renderShipRadarData(HttpExchange exchange, String shipID) throws SQLException, IOException {
+        shipsRadarData = database.getShipRadarData(shipID);
+        StringBuilder rows = new StringBuilder();
+        for (Map<String, Object> shipRadarData : shipsRadarData) {
+            rows.append(String.format("""
+            <tr>
+                <td data-label='Sector ID'>%s</td>
+                <td data-label='Height'>%s</td>
+                <td data-label='Ground'>%s</td>
+                <td data-label='Navigable'>%s</td>
+            </tr>
+            """,
+                    shipRadarData.get("sectorID"),
+                    shipRadarData.get("height"),
+                    shipRadarData.get("ground"),
+                    shipRadarData.get("navigable")
+            ));
+        }
+
+        String html = getHTMLBase("shipRadarData.html", rows);
+        renderHTML(exchange, html);
+    }
+
+    private void renderShips(HttpExchange exchange) throws SQLException, IOException {
+        ships = database.getAllShips();
+        StringBuilder rows = new StringBuilder();
+        for (Map<String, Object> ship : ships) {
+            rows.append(String.format("""
+                <tr>
+                    <td data-label='Ship ID'>%s</td>
+                    <td data-label='Name'>%s</td>
+                    <td data-label='Type'>%s</td>
+                    <td data-label='Active'>%s</td>
+                    <td data-label='Ship data'>
+                        <div class="button-container">
+                            <a class="back-button" href="http://localhost:8000/ship/%s/scan-results">Show ship scan data</a>
+                            <a class="back-button" href="http://localhost:8000/ship/%s/radar-results">Show ship radar data</a>
+                        </div>
+                    </td>
+                </tr>
+                """,
+                    ship.get("shipID"),
+                    ship.get("name"),
+                    ship.get("typ"),
+                    ship.get("active"),
+                    ship.get("shipID"),
+                    ship.get("shipID")
+            ));
+        }
+
+        String html = getHTMLBase("shipData.html", rows);
+        renderHTML(exchange, html);
+    }
+
+    private void renderShipScanData(HttpExchange exchange, int shipID) throws SQLException, IOException {
+        shipsScanData = database.getShipScanData(shipID);
+        StringBuilder rows = new StringBuilder();
+        for (Map<String, Object> shipScanData : shipsScanData) {
+            rows.append(String.format("""
+            <tr>
+                <td data-label='Sector ID'>%s</td>
+                <td data-label='Depth Average'>%s</td>
+                <td data-label='Standard Deviation'>%s</td>
+            </tr>
+            """,
+                    shipScanData.get("sectorID"),
+                    shipScanData.get("total_depth_average"),
+                    shipScanData.get("standard_deviation")
+            ));
+        }
+
+        String html = getHTMLBase("shipScanData.html", rows);
+        renderHTML(exchange, html);
+    }
+
+    public void renderSubmarineData(HttpExchange exchange) throws SQLException, IOException {
+        submarinesData = database.getAllSubmarineData();
+        StringBuilder rows = new StringBuilder();
+        for (Map<String, Object> submarineData : submarinesData) {
+            rows.append(String.format("""
+            <tr>
+                <td data-label='Submarine ID'>%s</td>
+                <td data-label='Ship name'>%s</td>
+                <td data-label='Sink position x'>%s</td>
+                <td data-label='Sink position y'>%s</td>
+                <td data-label='Sink position z'>%s</td>
+                <td data-label='Arise position x'>%s</td>
+                <td data-label='Arise position y'>%s</td>
+                <td data-label='Arise position z'>%s</td>
+                <td data-label='Active'>%s</td>
+                <td data-label='Sunk'>%s</td>
+            </tr>
+            """,
+                    submarineData.get("submarineID"),
+                    submarineData.get("shipName"),
+                    submarineData.get("sinkPositionX"),
+                    submarineData.get("sinkPositionY"),
+                    submarineData.get("sinkPositionZ"),
+                    submarineData.get("arisePositionX"),
+                    submarineData.get("arisePositionY"),
+                    submarineData.get("arisePositionZ"),
+                    submarineData.get("active"),
+                    submarineData.get("sunk")
+            ));
+        }
+
+        String html = getHTMLBase("submarineData.html", rows);
+        renderHTML(exchange, html);
+    }
+}
